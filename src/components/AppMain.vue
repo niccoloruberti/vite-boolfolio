f8c85324633866d95b29c6c9d5fb7aa6da3f5837
niccoloruberti <nikiruberti@gmail.com>
2023-08-29 18:14:40 +0200

    visualizzazione dei progetti a schermo

---
+++ src/components/AppMain.vue
@@ -0,0 +1,61 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'AppMain',
+    data() {
+        return{
+            baseUrl: 'http://localhost:8000',
+            projects: [],
+        }
+    },
+    created(){
+        this.getProjects();
+    },
+    methods:{
+        getProjects() {
+            axios.get(`${this.baseUrl}/api/projects`).then((response) => {
+                if(response.data.success) {
+                    this.projects = response.data.results;
+                } else {
+
+                }
+            })
+        }
+    }
+}
+</script>
+
+<template>
+  <div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Boolfolio</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4" v-for="project in projects" :key="project.id">
+                <div class="card">
+                    <div class="card-image-top">
+                        <img class="img-height img-fluid" :src="`${baseUrl}/storage/${project.img}`">
+                    </div>
+                    <div class="card-header">{{ project.name }}</div>
+                    <div class="card-body">
+                        Argomento: <span>{{ project.topic }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+.img-height {
+    max-height: 300px;
+}
+
+</style>
