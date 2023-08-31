253fc10b6dc64bf85a02e40e30519d9b73180852
niccoloruberti <nikiruberti@gmail.com>
2023-08-31 22:03:33 +0200

    aggiunta pagina 404

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'NotFound'
+    
+}
+</script>
+
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="mt-5 col-12 text-center">
+                <h1>ERRORE 404</h1>
+                <h2>Pagina non trovata</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios'
 import { store } from '../store.js'
+import { router } from '../router.js'
 export default {
     name: 'SingleProject',
     data() {
@@ -15,8 +16,13 @@ export default {
     methods: {
         getSinglesProject() {
             axios.get(`${this.store.baseUrl}/api/projects/${this.$route.params.slug}`).then((response) => {
+                console.log(response.data.success);
+
                 if(response.data.success) {
+                    console.log(response.data.success);
                     this.project = response.data.project;
+                } else {
+                    this.$router.push({name: 'not-found'});
                 }
             })
         }
