204187b0bdb87cc7ce7f3f900dcdd9f22ca1460a
niccoloruberti <nikiruberti@gmail.com>
2023-09-02 10:01:44 +0200

    completato contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -10,7 +10,7 @@ export default {
             email: '',
             message: '',
             success: false,
-            errors: {}
+            errors: []
         }
     },
     components: {
@@ -22,14 +22,15 @@ export default {
                 name: this.name,
                 email: this.email,
                 content: this.message,
-            }
+            };
             // pulisco l'array degli errori
-            this.errors = {};
+            this.errors = [];
 
             axios.post(`${this.store.baseUrl}/api/contacts`, data).then((response) => {
                 this.success = response.data.success;
                 if(!this.success) {
                     this.errors = response.data.errors;
+                    console.log(this.errors);
                 } else {
                     //ripulisco i dati in inpute
                     this.name = '',
@@ -57,27 +58,21 @@ export default {
                     </div>
                     <div class="row">
                         <div class="col-12">
+                            <p v-for="(error, index) in errors" :key="index" class="text-danger">
+                                        {{ error }}
+                            </p>
                             <form @submit.prevent="sendForm()" class="row">
                                 <div class="col-12 col-md-6">
                                     <label class="control-label">Nome e Cognome</label>
                                     <input type="text" name="name" id="name" v-model="name" placeholder="Name" class="form-control" :class="errors.name ? 'is-invalid' : ''">
-                                    <p v-for="(error, index) in errors" :key="index" class="text-danger">
-                                        {{ error }}
-                                    </p>
                                 </div>
                                 <div class="col-12 col-md-6">
                                     <label class="control -label">Email</label>
                                     <input type="mail" name="email" id="email" v-model='email' placeholder="Email" class="form-control" :class="errors.email ? 'is-invalid' : ''">
-                                    <p v-for="(error, index) in errors" :key="index" class="text-danger">
-                                        {{ error }}
-                                    </p>
                                 </div>
                                 <div class="col-12">
                                     <label class="contorl-label">Contenuto</label>
                                     <textarea class="form-control" name="content" id="content" v-model="message" placeholder="Messaggio" :class="errors.content ? 'is-invalid' : ''" cols="30" rows="10"></textarea>
-                                    <p v-for="(error, index) in errors" :key="index" class="text-danger">
-                                        {{ error }}
-                                    </p>
                                 </div>
                                 <div class="col-12">
                                     <button class="btn btn-success" type="submit">Invia</button>
